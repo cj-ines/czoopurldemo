aaf4772b475d2ff9cc7ebaa2f1f7a935196c3f3d
Crisdel James Ines <cj.ines@outlook.ph>
2014-06-21 11:25:35 +0800

    removing czoopurl

--- config/application.config.php
+++ config/application.config.php
@@ -5,7 +5,7 @@ return array(
         'Application',
 		'DoctrineModule',
 		'DoctrineORMModule',
-		'CzoopUrl',
+		//'CzoopUrl',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
