1be757cf26513d324d9278e541a49c3d980a93b0
Crisdel James Ines <cj.ines@outlook.ph>
2014-06-21 11:41:18 +0800

    fixing database

---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            // default connection name
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'mysqlsdb.co8hm2var4k9.eu-west-1.rds.amazonaws.com',
+                    'port'     => '3306',
+                    'user'     => 'depq8rv5h9d',
+                    'password' => 'pcWts4Js1DAo',
+                    'dbname'   => 'depq8rv5h9d',
+                )
+            )
+        )
+    ),
+);
